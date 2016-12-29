cb2d9c18ea9244babff4352f64100371710d0b23
Migadepan <conchicarcedo@correo.ugr.es>
2016-12-29 14:17:46 +0100

    Primera subida

---
+++ app/src/main/java/com/example/migadepan/tutorizate/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.migadepan.tutorizate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText usuario = (EditText) findViewById(R.id.correo_login);
+        final EditText password = (EditText) findViewById(R.id.password_login);
+
+        Button btn_entrar = (Button) findViewById(R.id.entrar_login);
+        btn_entrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/migadepan/tutorizate/RegistroAlumnoActivity.java
@@ -0,0 +1,13 @@
+package com.example.migadepan.tutorizate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroAlumnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_alumno);
+    }
+}
